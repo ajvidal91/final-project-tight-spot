ef5073f2a5fa18585f8435ff0148ec4787949fb5
ajvidal91 <angelletti21@gmail.com>
2018-03-30 09:56:39 -0400

    added backend functionality such as login auth and routes

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://letti:nyco1234@ds127899.mlab.com:27899/tight-spot' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
