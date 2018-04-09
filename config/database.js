b871e72cc6ef7c82e640d0b4b77de12fbfe3790f
ajvidal91 <angelletti21@gmail.com>
2018-04-09 17:37:27 -0400

    FULLY FUNCTIONAL!

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://letti:nyco1234@ds127899.mlab.com:27899/tight-spot' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
